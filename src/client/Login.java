bfadcb3e51e48b6526bc59c3ea3da5bc003339e3
AsiriM2000 <asiriambagahawaththa@gmail.com>
2022-08-06 21:22:03 +0530

    change login form

--- src/client/Login.java
+++ src/client/Login.java
@@ -1,5 +1,12 @@
 package client;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class Login {
 
+    public TextField clientName;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
 }
