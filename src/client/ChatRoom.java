272d08ec076b393e5b04229b6f1adfffaa940c34
AsiriM2000 <asiriambagahawaththa@gmail.com>
2022-08-08 13:56:00 +0530

    client message ui design

--- src/client/ChatRoom.java
+++ src/client/ChatRoom.java
@@ -1,5 +1,22 @@
 package client;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class ChatRoom {
 
+    public TextArea message_area;
+    public TextField message_type;
+
+    Socket socket;
+    DataInputStream inputStream;
+    DataOutputStream outputStream;
+
+    public void message_send_OnAction(ActionEvent actionEvent) {
+    }
 }
